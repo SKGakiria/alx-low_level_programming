9e1dcca4f4d55409226734d5d54a604fc1118a43
SKGakiria <swgakiria@gmail.com>
2022-12-10 01:00:31 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 /**
- *
  * main - Entry point
  *
  * Return: always 0
@@ -12,7 +11,7 @@ int main(void)
 
 	for (n1 = 0; n1 < 10; n1++)
 	{
-		for (n2 = n1 + 1, n2 < 10; n2++;)
+		for (n2 = n1 + 1; n2 < 10; n2++)
 		{
 			putchar(n1 + '0');
 			putchar(n2 + '0');
