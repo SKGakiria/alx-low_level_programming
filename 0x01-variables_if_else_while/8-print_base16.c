4cda02a76be4b5e2a1e8372ee4088f6cefed8c7e
SKGakiria <swgakiria@gmail.com>
2022-12-09 17:53:58 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -10,12 +10,12 @@ int main(void)
 
 	for (c - '0'; c <= '9'; c++)
 	{
-		putchar(0);
+		putchar(c);
 	}
 	for (c = 'a'; c <= 'f'; c++)
 	{
 		putchar(c);
 	}
 	putchar('\n');
-	return (0)
+	return (0);
 }
