21494090b69fb2e427959503e5b8d9c37d924b01
SKGakiria <swgakiria@gmail.com>
2023-01-09 03:42:04 -0800

    libmy.a

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - function to copy src to dest
+ * @n: number of bytes
+ * @src: string to be copied
+ * @dest: string to be copied to
+ *
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
