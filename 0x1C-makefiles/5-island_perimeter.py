e86a873f6e31d9c141e5146f7890bf42c03a0a74
SKGakiria <swgakiria@gmail.com>
2023-05-24 11:03:34 -0700

    Updated 5-island_perimeter.py file

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,8 +7,8 @@ def island_perimeter(grid):
     """Function that returns the perimeter of the island described in grid."""
 
     perimeter = 0
-    for i in range(0, len(grid)):
-        for j in range(0, len(grid)):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
             if grid[i][j]:
                 perimeter += surrounded_water(grid, i, j)
 
@@ -18,12 +18,16 @@ def surrounded_water(grid, i, j):
     """Function that returns num of surrounded water for a cell in grid."""
 
     water_count = 0
+
     if i <= 0 or not grid[i - 1][j]:
         water_count += 1
+
     if j <= 0 or not grid[i][j - 1]:
         water_count += 1
+
     if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
         water_count += 1
+
     if i >= len(grid) - 1 or not grid[i + 1][j]:
         water_count += 1
 
