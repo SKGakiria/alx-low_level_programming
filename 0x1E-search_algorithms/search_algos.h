b2dc3b983770afa5c261cfae57c585b4c873ea21
SKGakiria <swgakiria@gmail.com>
2023-07-12 06:39:23 -0700

    Updated Task 0

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,5 @@
-#ifndef SEARCH_ALGOS_H_
-#define SEARCH_ALGOS_H_
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -38,6 +38,7 @@ typedef struct skiplist_s
     struct skiplist_s *next;
     struct skiplist_s *express;
 } skiplist_t;
+
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
